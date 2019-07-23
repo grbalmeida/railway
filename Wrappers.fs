1f6f56dc577af813cf72f00e04a7163734b0c1e0
gilvan <g.r.almeida@live.com>
2019-07-22 21:47:58 -0300

    File

--- Wrappers.fs
+++ Wrappers.fs
@@ -8,3 +8,15 @@ module Json =
 
     let deserialize<'a> json =
         JsonConvert.DeserializeObject<'a>(json)
+      
+module File =
+    open System.IO
+
+    let save path content =
+        File.WriteAllText(path, content)
+
+    let openFile path =
+        File.ReadAllText(path)
+
+    let exists path =
+        File.Exists(path)
