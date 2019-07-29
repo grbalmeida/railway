8db339d96f2d4a165c687a7c0fe356eb7f1e1f9c
root <g.r.almeida@live.com>
2019-07-28 22:49:06 -0300

    Adding responses module

--- Transport.fs
+++ Transport.fs
@@ -13,3 +13,33 @@ module Filters =
         Description: string
         MaximumPrice: double
     }
+
+module Responses =
+    open Domain
+    open Operators
+
+    type CustomerResponse = {
+        Id: int
+        FullName: string
+        CPF: string
+        Age: int
+        Phone: string
+        Address: string
+    } with
+        static member
+            Transform (customer : Customer) =
+            {
+                Id = customer.Id
+                FullName = customer.Name ^ " " ^ customer.LastName
+                CPF = customer.CPF
+                Age = customer.Age
+                Phone = customer.Phone
+                Address = customer.Address
+            }
+
+    type PurchaseResponse = {
+        Id: int
+        Customer: CustomerResponse
+        Items: PurchaseItem list
+        Amount: double
+    }
